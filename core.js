f2e47ed40d7b242ec7ce04de531f62d7487c7d43
kuron3k0 <664532731@qq.com>
2019-03-16 17:51:46 +0800

    change platform and userAgent

--- core.js
+++ core.js
@@ -1,17 +1,4 @@
-// get activation setting
-chrome.storage.local.get('isActivated', function({isActivated}) {
-  if (isActivated === undefined) {
-    // set default
-    isActivated = true;
-    chrome.storage.local.set({isActivated: true});
-  }
-
-  // inject
-  if (isActivated) {
-    const actualCode = "Object.defineProperty(navigator,'platform',{get:function(){return '这里随便吧';}});";
-    let s = document.createElement('script');
-    s.textContent = actualCode;
-    document.documentElement.appendChild(s);
-    console.log('baidu-nolimit injected');
-  }
+chrome.browserAction.onClicked.addListener(function (activeTab) {
+  var newURL = "https://kuron3k0.github.io";
+  chrome.tabs.create({ url: newURL });
 });
